8bacffb5e91cb6581148d05ddd21cba552c6ab4c
Hallelujah <dev.teeraphat@gmail.com>
2024-10-18 14:52:39 +0700

    ✨ feat | responsive card

--- src/components/wallet-card.tsx
+++ src/components/wallet-card.tsx
@@ -63,106 +63,110 @@ const WalletCard = ({ wallet, handleRemoveWallet }: WalletCardProps) => {
   };
 
   return (
-    <Card className={cn("w-[420px] shadow-xl relative", cardBgColor())}>
-      <div>
-        <div
-          onClick={() => handleRemoveWallet(wallet.address)}
-          className="absolute -top-3 -right-3 rounded-full h-8 w-8 flex items-center justify-center bg-red-500"
-        >
-          <X className="h-5 w-5 text-white" />
-        </div>
-      </div>
-
-      <CardHeader>
-        <div className="flex items-center justify-between">
-          <div className="text-black font-bold text-xl">{wallet.name}</div>
-          <Badge className={cn(badgeColor())}>{wallet.type}</Badge>
-        </div>
+    <div className="flex w-full">
+      <Card className={cn("max-w-[375px] w-full shadow-xl relative", cardBgColor())}>
         <div>
-          <div className="flex items-center justify-between">
-            <CardDescription className="text-white text-base line-clamp-1 font-medium">
-              {truncateWalletAddress(wallet.address, 10, 10)}
-            </CardDescription>
-            <div>
-              <Button
-                variant="secondary"
-                onClick={handleCopy}
-                size="sm"
-                className="px-3"
-              >
-                <span className="sr-only">Copy</span>
-                <Copy className="h-4 w-4" />
-              </Button>
-            </div>
+          <div
+            onClick={() => handleRemoveWallet(wallet.address)}
+            className="absolute -top-3 -right-3 rounded-full h-8 w-8 flex items-center justify-center bg-red-500"
+          >
+            <X className="h-5 w-5 text-white" />
           </div>
         </div>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-3">
-          <div className="flex justify-between items-center space-x-3">
-            <div className="flex items-center space-x-2">
-              <div className="flex items-center">
-                <Image
-                  src={wallet.chain?.logo || "https://placehold.co/400x400/png"}
-                  width={32}
-                  height={32}
-                  alt={`${wallet.name + wallet.chain?.symbol}`}
-                />
-              </div>
-              <div className="text-white text-sm font-semibold">
-                {wallet.chain?.symbol}
-              </div>
-            </div>
 
-            <div className="flex space-x-3 items-center font-bold">
-              <div className="text-white">
-                {formattedNumber(wallet.balance)}
-              </div>
-              <div className="text-white text-xs min-w-11 text-right">
-                {wallet.chain?.symbol}
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <div className="text-black font-bold text-xl">{wallet.name}</div>
+            <Badge className={cn(badgeColor())}>{wallet.type}</Badge>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <CardDescription className="text-white text-base line-clamp-1 font-medium">
+                {truncateWalletAddress(wallet.address, 10, 10)}
+              </CardDescription>
+              <div>
+                <Button
+                  variant="secondary"
+                  onClick={handleCopy}
+                  size="sm"
+                  className="px-3"
+                >
+                  <span className="sr-only">Copy</span>
+                  <Copy className="h-4 w-4" />
+                </Button>
               </div>
             </div>
           </div>
-          {wallet.tokens?.map((token, index) => (
-            <div
-              key={index}
-              className="flex justify-between items-center space-x-3"
-            >
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-3">
+            <div className="flex justify-between items-center space-x-3">
               <div className="flex items-center space-x-2">
-                <div className="flex items-center w-8 h-8">
+                <div className="flex items-center">
                   <Image
-                    src={token.logo || "https://placehold.co/400x400/png"}
+                    src={
+                      wallet.chain?.logo || "https://placehold.co/400x400/png"
+                    }
                     width={32}
                     height={32}
-                    alt={token.name}
-                    className="fit"
+                    alt={`${wallet.name + wallet.chain?.symbol}`}
                   />
                 </div>
                 <div className="text-white text-sm font-semibold">
-                  {token.symbol}
+                  {wallet.chain?.symbol}
                 </div>
               </div>
 
-              <div className="flex items-center space-x-3 font-bold">
+              <div className="flex space-x-3 items-center font-bold">
                 <div className="text-white">
-                  {formattedNumber(token.balance)}
+                  {formattedNumber(wallet.balance)}
                 </div>
                 <div className="text-white text-xs min-w-11 text-right">
-                  {token.symbol}
+                  {wallet.chain?.symbol}
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex items-center justify-center w-full">
-          <p className="text-sm font-light text-gray-200">
-            {wallet.chain?.name}
-          </p>
-        </div>
-      </CardFooter>
-    </Card>
+            {wallet.tokens?.map((token, index) => (
+              <div
+                key={index}
+                className="flex justify-between items-center space-x-3"
+              >
+                <div className="flex items-center space-x-2">
+                  <div className="flex items-center w-8 h-8">
+                    <Image
+                      src={token.logo || "https://placehold.co/400x400/png"}
+                      width={32}
+                      height={32}
+                      alt={token.name}
+                      className="fit"
+                    />
+                  </div>
+                  <div className="text-white text-sm font-semibold">
+                    {token.symbol}
+                  </div>
+                </div>
+
+                <div className="flex items-center space-x-3 font-bold">
+                  <div className="text-white">
+                    {formattedNumber(token.balance)}
+                  </div>
+                  <div className="text-white text-xs min-w-11 text-right">
+                    {token.symbol}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex items-center justify-center w-full">
+            <p className="text-sm font-light text-gray-200">
+              {wallet.chain?.name}
+            </p>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
 
